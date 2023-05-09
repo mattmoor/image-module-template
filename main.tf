f8b5ab405a75dd5801ea1f62ab88e4c4ff35f261
Matt Moore <mattmoor@chainguard.dev>
2023-05-09 14:48:12 -0700

    Use the terraform module (#1)

---
+++ main.tf
@@ -0,0 +1,37 @@
+/*
+Copyright 2023 Chainguard, Inc.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+terraform {
+  required_providers {
+    cosign = {
+      source = "chainguard-dev/cosign"
+    }
+    apko = {
+      source = "chainguard-dev/apko"
+    }
+  }
+}
+
+variable "target_repository" {
+  description = "The docker repo into which the image and attestations should be published."
+}
+
+provider "apko" {
+  extra_repositories = ["https://packages.wolfi.dev/os"]
+  extra_keyring      = ["https://packages.wolfi.dev/os/wolfi-signing.rsa.pub"]
+  default_archs      = ["x86_64", "aarch64"]
+  extra_packages     = ["wolfi-baselayout"]
+}
+
+module "image" {
+  source  = "github.com/chainguard-dev/terraform-publisher-apko//"
+
+  target_repository = var.target_repository
+  config = file("${path.module}/apko.yaml")
+}
+
+output "image_ref" {
+  value = module.image.image_ref
+}
